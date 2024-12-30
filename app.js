707d145db07c9732c05fde1f226638303afe70e7
Emperor-1231 <abdoakojdad@gmail.com>
2024-12-30 13:58:56 +0100

    app.js

--- app.js
+++ app.js
@@ -1,35 +1,22 @@
-// استيراد المكتبات
-const express = require('express');
+// استيراد المكتبات اللازمة
+const express = require("express");
+const path = require("path");
+
+// إنشاء تطبيق Express
 const app = express();
-const path = require('path');
 
-// إعدادات Firebase
-const { initializeApp } = require("firebase/app");
-const { getAuth, signInWithEmailAndPassword, signInWithPopup, GoogleAuthProvider } = require("firebase/auth");
+// تحديد المنفذ الذي سيعمل عليه الخادم
+const port = process.env.PORT || 3000;
 
-const firebaseConfig = {
-  apiKey: "AIzaSyC0jbvoclp54WAeKPEjwL9TqLme1FETK9Q",
-  authDomain: "el-hardi-auto-publish.firebaseapp.com",
-  projectId: "el-hardi-auto-publish",
-  storageBucket: "el-hardi-auto-publish.firebasestorage.app",
-  messagingSenderId: "804047155992",
-  appId: "1:804047155992:web:408a8aea01af1481dca46d"
-};
+// تقديم الملفات الثابتة من مجلد public
+app.use(express.static(path.join(__dirname, "public")));
 
-// تهيئة Firebase
-const firebaseApp = initializeApp(firebaseConfig);
-const auth = getAuth(firebaseApp);
-
-// إعداد مسار الواجهة
-app.use(express.static(path.join(__dirname, 'public')));
-
-// عرض صفحة index.html
-app.get('/', (req, res) => {
-  res.sendFile(path.join(__dirname, '/views/index.html'));
+// المسار الرئيسي الذي يقدم index.html
+app.get("/", (req, res) => {
+  res.sendFile(path.join(__dirname, "views", "index.html"));
 });
 
-// تشغيل السيرفر
-const port = 3000;
+// بدء الخادم
 app.listen(port, () => {
-  console.log(`Server running on http://localhost:${port}`);
+  console.log(`Server is running on port ${port}`);
 });
