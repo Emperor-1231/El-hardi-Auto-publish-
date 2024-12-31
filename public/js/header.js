312815f3d11bbb457bf85c894c451c179d6cafbc
Emperor-1231 <abdoakojdad@gmail.com>
2024-12-31 13:26:38 +0100

    header.js

---
+++ public/js/header.js
@@ -0,0 +1,6 @@
+// public/CSS/header.js
+
+// يمكنك إضافة أي وظائف JavaScript إذا كانت هناك حاجة لها
+document.addEventListener('DOMContentLoaded', function () {
+    console.log('Header loaded successfully!');
+});
