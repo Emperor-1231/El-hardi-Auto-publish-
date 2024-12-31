a57fe1a468b9e0af369db25a0beb6f3796ebd9c3
Emperor-1231 <abdoakojdad@gmail.com>
2024-12-31 13:29:38 +0100

    footer.js

---
+++ public/js/footer.js
@@ -0,0 +1,6 @@
+// public/js/footer.js
+
+// Example of a footer JavaScript function, you can add more functionality as needed
+document.addEventListener("DOMContentLoaded", function () {
+    console.log("Footer loaded successfully!");
+});
